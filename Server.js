ba6717a8a6eb05c5135e01a87d4cfde84b2c18db
dilnathbro <dbw775318@gmail.com>
2026-09-05 23:12:19 +0530

    Update and rename Server.js to sample

--- Server.js
+++
@@ -1,8 +0,0 @@
-// Validating multiple platforms
-const allowedDomains = ['facebook.com', 'fb.watch', 'youtube.com', 'youtu.be', 'instagram.com', 'tiktok.com', 'x.com', 'twitter.com'];
-
-const isSupported = allowedDomains.some(domain => url.includes(domain));
-
-if (!url || !isSupported) {
-  return res.status(400).json({ error: 'කරුණාකර සහාය දක්වන (FB, YT, Insta, TikTok) නිවැරදි Link එකක් ලබා දෙන්න.' });
-}
